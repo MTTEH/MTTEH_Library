ef184b58896c7c1ba275a200096c10d0a2320b7a
GGLinnk <gglinnk@protonmail.com>
2020-09-19 09:11:54 +0200

    Update mtteh.h
    Add drivers PIN defines

--- mtteh.h
+++ mtteh.h
@@ -4,4 +4,13 @@
 #define program_died() while (1)
 #define wait_for(x) while (!x)
 
+#define MAvD1 2
+#define MAvD2 3
+#define MAvG1 4
+#define MAvG2 5
+#define MArD1 6
+#define MArD2 7
+#define MArG1 8
+#define MArG2 9
+
 #endif //MTTEH_H_
